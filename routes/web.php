45eae6a4b6e07147088346caef106756961aa83c
João Fernandes <joaovitorfernandes2@gmail.com>
2017-05-11 11:42:32 -0300

    lesson-74

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('{provider}/auth', 'SocialController@auth')
+    ->name('social.auth');
+
+Route::get('{provider}/redirect', 'SocialController@auth_callback')
+    ->name('social.callback');
